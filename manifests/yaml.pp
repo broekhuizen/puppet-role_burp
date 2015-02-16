83a34d65ed5b4df7d0a515b8a163b1501e724179
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-16 11:17:28 +0100

    Update yaml.pp

--- manifests/yaml.pp
+++ manifests/yaml.pp
@@ -13,41 +13,59 @@ class role_burp::yaml (
   $cname              = $hostname,
   $server_can_restore = "1",
 
-# client: create client config files in /etc/clientconfdir for Linux clients
 burp::client::includes:  
-  "/home"
+  /home
   
 burp::client::excludes: 
-  "/tmp"
-  $options  = "",
-  $password = "password",
-  $cron     = true,
+  /tmp
 
-# server: settings for /etc/burp-server.conf
-  $directory           = "/mnt/backup/burpdata",
-  $max_children        = "25",
-  $max_status_children = "25",
-  $keep                = "100",
-  $waittime            = "20h",
-  $starttime           = "Mon,Tue,Wed,Thu,Fri,Sat,Sun,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23",
-  $common_clientconfig = ['working_dir_recovery_method=resume'],
+burp::client::options:
 
-# server: create client config files in /etc/clientconfdir for Windows clients
-  $clientconf_hash     = { 'servername-01.domain' => { includes => ['C:/', 'D:/'],
-                                                       excludes => ['D:/$RECYCLE.BIN/'],
-                                                       options  => ['options'],
-                                                       password => 'password',
-                                                     },
+burp::client::password
+  password
+  
+burp::client::cron
+  true
 
-                           'servername-02.domain' => { includes => ['C:/', 'D:/'],
-                                                       excludes => ['D:/$RECYCLE.BIN/'],
-                                                       options  => ['options'],
-                                                       password => 'password',
-                                                     },
-                         },
-) {
+burp::server::directory 
+  /mnt/backup/burpdata
+  
+burp::server::max_children      
+  25
 
+burp::server::max_status_children
+  25
 
+burp::server::keep
+  100
+
+burp::server::waittime
+  20h
+
+burp::server::starttime
+  Mon,Tue,Wed,Thu,Fri,Sat,Sun,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23
+
+burp::server::common_clientconfig
+  working_dir_recovery_method=resume
+
+burp::server::clientconf_hash:
+  servername-01.domain:
+    includes:
+      - C:/
+      - D:/
+    excludes:
+      - D:/$RECYCLE.BIN/
+    options:
+    password: password
+
+  servername-02.domain:
+    includes:
+      - C:/
+      - D:/
+    excludes:
+      - D:/$RECYCLE.BIN/
+    options:
+    password: password
 ---
 ') {
 
