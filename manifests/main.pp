a272a8fb12f0de090967de0e3efdd1b0480809e7
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-17 13:18:20 +0100

    Update main.pp

--- manifests/main.pp
+++ manifests/main.pp
@@ -3,10 +3,12 @@
 #
 class role_burp::main (
   $mode            = $role_burp::yaml::parameters['role_burp::main::mode'],
+  $password        = $role_burp::yaml::parameters['role_burp::main::password'],
   $clientconf_hash = $role_burp::yaml::parameters['role_burp::main::clientconf_hash'],
   ) {
   class { 'burp':
     mode            => $mode,
+    password        => $password,
     clientconf_hash => $clientconf_hash,
   }
   
