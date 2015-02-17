ae8bdf896b19973b7a71c1236e287ad6d7c71060
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-17 11:40:58 +0100

    Update main.pp

--- manifests/main.pp
+++ manifests/main.pp
@@ -1,8 +1,10 @@
 # == Class: role_burp::main
 #
 #
-class role_burp::main {
-  
+class role_burp::main (
+  $server = $role_burp::yaml::parameters['role_burp::server'],
+  ) {
   class { 'burp': }
+    server => $server,
  
 }
