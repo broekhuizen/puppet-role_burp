5c43236a0eb78ec9f983cdca61560aec17972133
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-17 13:09:35 +0100

    Update main.pp

--- manifests/main.pp
+++ manifests/main.pp
@@ -2,7 +2,7 @@
 #
 #
 class role_burp::main (
-  $mode = $role_burp::yaml::parameters['role_burp::mode'],
+  $mode = $role_burp::yaml::parameters['role_burp::main::mode'],
   ) {
   class { 'burp':
     mode => $mode,
