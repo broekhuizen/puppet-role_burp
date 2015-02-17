65f7d17c9931be83c6b12497f2abcd15841cc240
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-17 11:36:23 +0100

    Update yaml.pp

--- manifests/yaml.pp
+++ manifests/yaml.pp
@@ -3,53 +3,24 @@
 class role_burp::yaml (
   $yaml = '
 ---
-burp::client::server
-  127.0.0.1
-
-burp::client::password
-  password
-
-burp::client::client_password
-  password
-  
-burp::client::cname
-
-burp::client::server_can_restore
-  1
-
-burp::client::includes:  
-  "/home"
-  
-burp::client::excludes: 
-  "/tmp"
-
-burp::client::options:
-
-burp::client::cron
-  true
-
-burp::server::directory 
-  "/mnt/backup/burpdata"
-  
-burp::server::max_children      
-  25
-
-burp::server::max_status_children
-  25
-
-burp::server::keep
-  100
-
-burp::server::waittime
-  20h
-
-burp::server::starttime
+burp::server: 127.0.0.1
+burp::password: password
+burp::server_can_restore: 1
+burp::includes: "/home"
+burp::excludes: "/tmp"
+burp::options:
+burp::cron: true
+burp::directory: "/mnt/backup/burpdata"
+burp::max_children: 25
+burp::max_status_children: 25
+burp::keep: 100
+burp::waittime: 20h
+burp::starttime
   Mon,Tue,Wed,Thu,Fri,Sat,Sun,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23
 
-burp::server::common_clientconfig
-  working_dir_recovery_method=resume
+burp::common_clientconfig: working_dir_recovery_method=resume
 
-burp::server::clientconf_hash:
+burp::clientconf_hash:
   servername-01.domain:
     includes:
       - "C:/"
