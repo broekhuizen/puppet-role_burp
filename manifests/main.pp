5fb8bc8b1e1d94c439cf8aa8de9bba00e1b89da8
broekhuizen <rudibroekhuizen@yahoo.com>
2015-02-20 13:26:52 +0100

    Update main.pp

--- manifests/main.pp
+++ manifests/main.pp
@@ -2,29 +2,31 @@
 #
 #
 class role_burp::main (
-  $mode                  = $role_burp::yaml::parameters['role_burp::main::mode'],
-  $directory             = $role_burp::yaml::parameters['role_burp::main::directory'],
-  $ssl_key_password      = $role_burp::yaml::parameters['role_burp::main::ssl_key_password'],
-  $password              = $role_burp::yaml::parameters['role_burp::main::password'],
-  $server                = $role_burp::yaml::parameters['role_burp::main::server'],
-  $clientconf_hash       = $role_burp::yaml::parameters['role_burp::main::clientconf_hash'],
-  $backup_stats_logstash = $role_burp::yaml::parameters['role_burp::main::backup_stats_logstash'],
-  $includes              = $role_burp::yaml::parameters['role_burp::main::includes'],
-  $excludes              = $role_burp::yaml::parameters['role_burp::main::excludes'],
-  $extra_options         = $role_burp::yaml::parameters['role_burp::main::extra_options'],
+  $directory               = $role_burp::yaml::parameters['role_burp::main::directory'],
+  $client_ssl_key_password = $role_burp::yaml::parameters['role_burp::main::client_ssl_key_password'],
+  $server_ssl_key_password = $role_burp::yaml::parameters['role_burp::main::server_ssl_key_password'],
+  $password                = $role_burp::yaml::parameters['role_burp::main::password'],
+  $server                  = $role_burp::yaml::parameters['role_burp::main::server'],
+  $clientconf_hash         = $role_burp::yaml::parameters['role_burp::main::clientconf_hash'],
+  $backup_stats_logstash   = $role_burp::yaml::parameters['role_burp::main::backup_stats_logstash'],
+  $includes                = $role_burp::yaml::parameters['role_burp::main::includes'],
+  $excludes                = $role_burp::yaml::parameters['role_burp::main::excludes'],
+  $client_extra_options    = $role_burp::yaml::parameters['role_burp::main::client_extra_options'],
+  $server_extra_options    = $role_burp::yaml::parameters['role_burp::main::server_extra_options'],
   ) {
   
   class { 'burp':
-    mode                  => $mode,
-    directory             => $directory,
-    ssl_key_password      => $ssl_key_password,
-    password              => $password,
-    server                => $server,
-    clientconf_hash       => $clientconf_hash,
-    backup_stats_logstash => $backup_stats_logstash,
-    includes              => $includes,
-    excludes              => $excludes,
-    extra_options         => $extra_options,
+    directory               => $directory,
+    client_ssl_key_password => $client_ssl_key_password,
+    server_ssl_key_password => $server_ssl_key_password,
+    password                => $password,
+    server                  => $server,
+    clientconf_hash         => $clientconf_hash,
+    backup_stats_logstash   => $backup_stats_logstash,
+    includes                => $includes,
+    excludes                => $excludes,
+    client_extra_options    => $client_extra_options,
+    server_extra_options    => $server_extra_options,
   }
   
 }
